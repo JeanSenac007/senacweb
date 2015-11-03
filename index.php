1a8c63d25cd641ca3a430531ded039ace2a0d438
JeanSenac007 <jikapieri@gmail.com>
2015-11-03 16:43:36 -0200

    modificado o index.php

--- index.php
+++ index.php
@@ -5,7 +5,22 @@ $app = new \Slim\Slim();
 
 // Hello World
 $app->get('/hello/:name', function ($name) {
-    echo "hello, " . $name;
-});
+    $inteiro1 = 42; // tipo integer
+    $inteiro2 = 79; // tipo integer
+    $texto = "Ola, "; // tipo string
+    $fracao = 2.3; // tipo float
+    
+    $soma = $inteiro1 + $inteiro2;
+    $divisao = $inteiro1 / $fracao;
+    $multi = $inteiro2 * $fracao;
+    
+    $olamundo = $texto . $name;
+    
+    echo "<P>Soma: " . $soma . "</p>";
+    echo "<P>Divisao: " . $divisao . "</p>";
+    echo "<P>Multiplica: " . $multi . "</p>";
+    echo "<P>" . $olamundo . "</p>";
+    
+    });
     
     $app->run();
